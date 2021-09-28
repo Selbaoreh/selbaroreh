8409e2632837a5961bfce27f3e06580a9b6b0be5
Selbaoreh <91538662+Selbaoreh@users.noreply.github.com>
2021-09-28 14:35:36 +0200

    Update script.js

--- script.js
+++ script.js
@@ -5,3 +5,22 @@ btn.addEventListener("click", () => {
     nav.classList.toggle("active");
     btn.classList.toggle("active");
 });
+
+const container = document.getElementById("container");
+const img = document.querySelector("img");
+
+
+container.addEventListener("mousemove", (e) => {
+    const x = e.clientX - e.target.offsetLeft;
+    const y = e.clientY - e.target.offsetTop;
+
+    console.log(x, y);
+
+    img.style.transformOrigin = `${x}px ${y}px`;
+    img.style.transform = "scale(3)";
+});
+
+container.addEventListener("mouseleave", () => {
+    img.style.transformOrigin = "center center";
+    img.style.transform = "scale(1)";
+});
